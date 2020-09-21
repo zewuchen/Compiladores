d7bb447e6f288ea5acd34b5d275398b3ec12aae0
Zewu Chen <zewu.chen@etec.sp.gov.br>
2020-09-21 18:43:19 -0300

    Adicionado cabeçalho de trabalho

--- analisador_lexico.c
+++ analisador_lexico.c
@@ -1,3 +1,23 @@
+/*
+    Entrega de trabalho - Mini Analisador Léxico
+
+    Nós,
+
+    Zewu Chen 31808751
+
+    declaramos que
+
+    todas as respostas são fruto de nosso próprio trabalho,
+    não copiamos respostas de colegas externos à equipe,
+    não disponibilizamos nossas respostas para colegas externos à equipe e
+    não realizamos quaisquer outras atividades desonestas para nos beneficiar ou prejudicar outros.
+
+    Matéria: Compiladores
+    Curso: Ciência da Computação
+    TIA: 31808751
+    Professor: Fabio Lubacheski
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
